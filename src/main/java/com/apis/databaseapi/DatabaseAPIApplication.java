5326ca0accf7f33c5fcf3d7e1540802bbea55679
prane <praneethchandra.chandra98@gmail.com>
2023-07-22 05:12:46 -0400

    database api project created

---
+++ src/main/java/com/apis/databaseapi/DatabaseAPIApplication.java
@@ -0,0 +1,12 @@
+package com.apis.databaseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseAPIApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(DatabaseAPIApplication.class, args);
+    }
+}
